7893880d5e390bf31c2e3a88ffea199e69e9f202
Ocholi Joseph <ocholijoseph@gmail.com>
2023-06-28 04:39:24 -0700

    Configuration Management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,14 @@
-# installs the package puppet-lint
-package { 'puppet-lint':
-  ensure   => '2.1.1',
-  provider => 'gem'
+# Puppet manifest to install Flask version 2.1.0 using pip3
+
+# Ensure that the pip3 package is installed
+package { 'python3-pip':
+  ensure => installed,
 }
+
+# Install Flask version 2.1.0
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
+}
+
