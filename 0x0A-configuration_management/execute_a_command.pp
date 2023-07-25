6eb86aa8e5c343133f86af730055434f5f35f8dc
Ocholi Joseph <ocholijoseph@gmail.com>
2023-07-25 13:32:49 -0700

    Grav in progress

--- 0x0A-configuration_management/execute_a_command.pp
+++
@@ -1,7 +0,0 @@
-# Puppet manifest to kill a process named killmenow using pkill
-
-exec { 'killmenow':
-  command     => 'pkill killmenow',
-  path        => '/usr/bin:/usr/sbin:/bin:/sbin',
-  refreshonly => true,
-}
